7ef763fe8afebddf980e903e8bd7639513404e4a
Noah Barnett <52221065+NoahBarnett06@users.noreply.github.com>
2020-12-09 22:44:02 +0000

    Add files via upload

---
+++ BorwellChallenge/Openings.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BorwellChallenge
+{
+    public partial class frmOpenings : Form
+    {
+        // Creates an instance of the Room class and loads data from the instance on the homepage
+        Room Room;
+
+        public frmOpenings(Room R)
+        {
+            InitializeComponent();
+            Room = R;
+        }
+
+        // Closes this form and brings focus back to the homepage form
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Opens the results form and passes the Room class instance as a parametre to the form
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            new frmResults(Room).Show();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Checks if any of the numbers entered are negative and if so, shows a message and stops the code from reading
+            if (("" + Room.Length).IndexOf('-') >= 0 || ("" + Room.Width).IndexOf('-') >= 0 || ("" + Room.Height).IndexOf('-') >= 0)
+            {
+                MessageBox.Show("Input cannot be a negative number");
+                return;
+            }
+
+            try
+            {
+                // Sets the opening height and width class properties to input in the related textboxes and also turns them into decimals
+                Room.OpeningHeight = decimal.Parse(txtOpeningHeight.Text);
+                Room.OpeningWidth = decimal.Parse(txtOpeningWidth.Text);
+
+                // Rounds the opening height and width class properties to 2 decimal places
+                Room.OpeningWidth = Math.Round(Room.OpeningWidth, 2);
+                Room.OpeningHeight = Math.Round(Room.OpeningHeight, 2);
+
+                // Displays an entry in the listbox including the opening number, height, width and area from the room class instance
+                lstOpenings.Items.Add("No. " + Room.OpeningNumber + ", Height: " + Room.OpeningHeight +
+                    " m, Width: " + Room.OpeningWidth + " m, Area: " + Room.Opening + " m²");
+
+                // Clears all of the textboxes
+                txtOpeningHeight.Text = "";
+                txtOpeningWidth.Text = "";
+            }
+
+            // Catches any errors based on the data entered by the user (if any data is actually entered and if it is a vallid number)
+            catch
+            {
+                if (txtOpeningHeight.Text == "" || txtOpeningWidth.Text == "")
+                {
+                    MessageBox.Show("Input was empty");
+                    return;
+                }
+                MessageBox.Show("Input was not a number");
+                return;
+            }
+        }
+    }
+}
---
+++ BorwellChallenge/Room.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorwellChallenge
+{
+    public class Room
+    {
+        // Publicly declares the variables used in the this class, and also sets their datatypes
+        public decimal Width;
+        public decimal Length;
+        public decimal Height;
+        public decimal OpeningHeight;
+        public decimal OpeningWidth;
+        public int OpeningCount;
+        public decimal Total;
+
+        // Calculates the area of the walls
+        public decimal Wall1
+        {
+            get { return Length * Height; }
+        }
+        public decimal Wall2
+        {
+            get { return Width * Height; }
+        }
+        public decimal WallTotal
+        {
+            get { return 2 * (Wall1 + Wall2); }
+        }
+
+        // Calculates the volume of the room
+        public decimal Volume
+        {
+            get { return Width * Length * Height; }
+        }
+        
+        // Calculate the area of the floor
+        public decimal FloorArea
+        {
+            get { return Width * Length; }
+        }
+
+        // Caclulates the area of the opening (door/window) and rounds it to two decimal places
+        public decimal Opening
+        {
+            get { return Math.Round(OpeningHeight * OpeningWidth, 2); }
+        }
+
+        // Counts the number of openings added
+        public int OpeningNumber
+        {
+            get { return OpeningCount += 1; }               
+        }
+
+        // Calculates the total area of all the openings
+        public decimal OpeningTotal
+        {
+            get
+            {
+                Total += Opening;
+                return Total;
+            }
+        }
+
+        // Calculates how many litres of paint are needed to cover the area of the walls using a ratio of 10:1 for area of the walls(m²) to litres of paint
+        public decimal Paint
+        {
+            get
+            {
+                var Walls = (WallTotal - OpeningTotal) / 10;
+                if (Walls < 1)
+                {
+                    return 1;
+                }
+                if (Walls > Math.Round(Walls))
+                {
+                    Walls = Math.Round(Walls);
+                    var x = Walls + 1;
+                    return x;
+                }
+                else
+                {
+                    Walls = Math.Round(Walls);
+                    var x = Walls;
+                    return x;
+                }
+            }
+        }
+        
+    }
+}
